c17ecb23ca274c00861fc7c5a8e8bb4f2782122c
saandypp <59781108+saandypp@users.noreply.github.com>
2022-05-12 01:44:04 +0200

    Expose endpoints for the CRUD functions.

--- src/main/java/com/example/demo/controller/CheckController.java
+++ src/main/java/com/example/demo/controller/CheckController.java
@@ -3,9 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.modules.Check;
 import com.example.demo.services.CheckService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,37 @@ public class CheckController {
     @GetMapping
     public List<Check> printAllCheck(){
         return checkService.getChecks();
+    }
+
+    @GetMapping("/{name}")
+    public Check getCheck(@PathVariable String name) {
+        Check theCheck = checkService.findByName(name);
+        if (theCheck == null) {
+            throw new RuntimeException("Check not found " + name);
+        }
+        return theCheck;
+    }
+
+    @PostMapping
+    public Check addCheck(@RequestBody Check theCheck) {
+        checkService.save(theCheck);
+        return theCheck;
+    }
+
+    @PutMapping
+    public Check updateCheck(@RequestBody Check theCheck) {
+        checkService.save(theCheck);
+        return theCheck;
+    }
+
+    @DeleteMapping("/{name}")
+    public String deleteEmployee(@PathVariable String name) {
+        Check theCheck = checkService.findByName(name);
+
+        if (theCheck == null) {
+            throw new RuntimeException("Check not found " + name);
+        }
+        checkService.deleteByName(name);
+        return "Deleted " + name;
     }
 }
