6f5d9f7bb8578d4f5865e11c16391ce2d470fa3d
Ne184 <nevena.gincheva@abv.bg>
2022-05-12 18:10:46 +0200

    Create ActionTypes

---
+++ src/main/java/com/example/demo/modules/ActionTypes.java
@@ -0,0 +1,19 @@
+package com.example.demo.modules;
+
+public enum ActionTypes {
+    NotEmpty,
+    Empty,
+    Contains,
+    NotContains,
+    StrictlyGreater,
+    StrictlySmaller,
+    GreaterEqual,
+    SmallerEqual,
+    LengthStrictlyGreater,
+    LengthStrictlySmaller,
+    LengthGreaterEqual,
+    LengthSmallerEqual,
+    DifferentAttrValue,
+    SameAttrValue,
+    HumanCheck
+}
