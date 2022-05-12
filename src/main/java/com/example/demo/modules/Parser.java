62087ac3eafb6a347b1785ab952756d76938d5f2
Hristo <h.bakardzhiev@gmx.com>
2022-05-12 18:15:54 +0200

    refactor

--- src/main/java/com/example/demo/modules/Parser.java
+++ src/main/java/com/example/demo/modules/Parser.java
@@ -9,7 +9,6 @@ import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 public class Parser implements ParserRepository {
 
--- src/main/java/com/example/demo/modules/SheetSource.java
+++ src/main/java/com/example/demo/modules/SheetSource.java
@@ -9,6 +9,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +24,7 @@ import org.hibernate.Hibernate;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Table
 public class SheetSource {
 
   @Id
@@ -41,10 +44,14 @@ public class SheetSource {
   @Column(nullable = true)
   private String name;
 
+
   @Column(nullable = false)
   @Enumerated
   @ElementCollection(targetClass=SheetType.class)
   private List<Enum<SheetType>> sheetSourceType;
+
+  @ManyToMany
+  private List<Rule> rules;
 
   public SheetSource(String htmlID, String type, List<Enum<SheetType>> sheetSourceType) {
     this.htmlID = htmlID;
@@ -52,139 +59,11 @@ public class SheetSource {
     this.sheetSourceType = sheetSourceType;
   }
 
-  public SheetSource(String htmlTag, String htmlID, String type, List<Enum<SheetType>> sheetSourceType) {
+  public SheetSource(String htmlTag, String htmlID, String type,
+      List<Enum<SheetType>> sheetSourceType) {
     this(htmlID, type, sheetSourceType);
     this.htmlTag = htmlTag;
   }
-
-//
-//  private String description;
-//  private String[] changeType;
-//  private boolean customerApproval;
-//  private boolean supplierApproval;
-//  private String philipsID;
-//  private Boolean changeAdminAuditReq;
-//  private String teamName;
-//  private String location;
-//  private String[] createdBy;
-//  private LocalDateTime createdOn;
-//  private String[] modifiedBy;
-//  private LocalDateTime modifiedOn;
-//  private Boolean regulatoryRestrictionReq;
-//
-//  public Long getId() {
-//    return id;
-//  }
-//
-//  public void setId(Long id) {
-//    this.id = id;
-//  }
-//
-//  public Boolean getChangeAdminAuditReq() {
-//    return changeAdminAuditReq;
-//  }
-//
-//  public Boolean getRegulatoryRestrictionReq() {
-//    return regulatoryRestrictionReq;
-//  }
-//
-//  public void setRegulatoryRestrictionReq(Boolean regulatoryRestrictionReq) {
-//    this.regulatoryRestrictionReq = regulatoryRestrictionReq;
-//  }
-//
-//  public void setChangeAdminAuditReq(Boolean changeAdminAuditReq) {
-//    this.changeAdminAuditReq = changeAdminAuditReq;
-//  }
-//
-//  public String getTeamName() {
-//    return teamName;
-//  }
-//
-//  public void setTeamName(String teamName) {
-//    this.teamName = teamName;
-//  }
-//
-//  public String getLocation() {
-//    return location;
-//  }
-//
-//  public void setLocation(String location) {
-//    this.location = location;
-//  }
-//
-//  public String[] getCreatedBy() {
-//    return createdBy;
-//  }
-//
-//  public void setCreatedBy(String[] createdBy) {
-//    this.createdBy = createdBy;
-//  }
-//
-//  public LocalDateTime getCreatedOn() {
-//    return createdOn;
-//  }
-//
-//  public void setCreatedOn(LocalDateTime createdOn) {
-//    this.createdOn = createdOn;
-//  }
-//
-//  public String[] getModifiedBy() {
-//    return modifiedBy;
-//  }
-//
-//  public void setModifiedBy(String[] modifiedBy) {
-//    this.modifiedBy = modifiedBy;
-//  }
-//
-//  public LocalDateTime getModifiedOn() {
-//    return modifiedOn;
-//  }
-//
-//  public void setModifiedOn(LocalDateTime modifiedOn) {
-//    this.modifiedOn = modifiedOn;
-//  }
-//
-//  public CN() {}
-//
-//  public String getPhilipsID() {
-//    return philipsID;
-//  }
-//
-//  public void setPhilipsID(String philipsID) {
-//    this.philipsID = philipsID;
-//  }
-//
-//  public String getDescription() {
-//    return description;
-//  }
-//
-//  public void setDescription(String description) {
-//    this.description = description;
-//  }
-//
-//  public String[] getChangeType() {
-//    return changeType;
-//  }
-//
-//  public void setChangeType(String[] changeType) {
-//    this.changeType = changeType;
-//  }
-//
-//  public boolean isCustomerApproval() {
-//    return customerApproval;
-//  }
-//
-//  public void setCustomerApproval(boolean customerApproval) {
-//    this.customerApproval = customerApproval;
-//  }
-//
-//  public boolean isSupplierApproval() {
-//    return supplierApproval;
-//  }
-//
-//  public void setSupplierApproval(boolean supplierApproval) {
-//    this.supplierApproval = supplierApproval;
-//  }
 
   @Override
   public boolean equals(Object o) {
