231c2c31a1ba3921a16e70b1047ede357af23127
Ne184 <nevena.gincheva@abv.bg>
2022-05-12 11:11:00 +0200

    Put some annotation for rep

--- src/main/java/com/example/demo/repository/CheckRepository.java
+++ src/main/java/com/example/demo/repository/CheckRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.modules.Check;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CheckRepository extends JpaRepository<Check, String> {
 
 }
