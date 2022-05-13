7e16dd5e69258765a7cb59db7d5ebc74fdb7956b
Hristo <h.bakardzhiev@gmx.com>
2022-05-13 16:16:48 +0200

    Revert "added attribute graph"
    This reverts commit 6d0a6ee3022fb47d59488b9692b9f9bbd4207273.

--- src/main/java/com/example/demo/configuration/CNConfiguration.java
+++ src/main/java/com/example/demo/configuration/CNConfiguration.java
@@ -6,22 +6,14 @@ import com.example.demo.modules.SheetType;
 import com.example.demo.repository.SheetSourceRepository;
 import com.example.demo.repository.ParserRepository;
 import java.io.IOException;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import org.jgrapht.Graph;
-import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.Multigraph;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
 
 @Configuration
 public class CNConfiguration {
@@ -31,47 +23,8 @@ public class CNConfiguration {
     return new Parser("Change Notice - Example.html", SheetType.CN);
   }
 
-
-  Graph<URI, DefaultEdge> g = new Multigraph<>(DefaultEdge.class);
-
-
-  private void addMultipleEdges(Graph g, String attribute) {
-    for (SheetType s: SheetType.values()) {
-      g.addEdge(s, attribute);
-    }
-  }
-
-  public List converter(Graph g, String attr) {
-    ArrayList<Enum<SheetType>> list = new ArrayList<>();
-    Set<DefaultEdge> edges = g.incomingEdgesOf(attr);
-    for (DefaultEdge e : edges) {
-      list.add((Enum<SheetType>) g.getEdgeSource(e));
-    }
-    return list;
-  }
-
-  public void populateGraph(Graph g) {
-    g.addVertex(SheetType.CN);
-    g.addVertex(SheetType.CT);
-    g.addVertex(SheetType.CR);
-    g.addVertex(SheetType.DMR);
-    g.addVertex("name");
-    g.addVertex("proposedSolution");
-    g.addVertex("customerApprovalRequired");
-    g.addVertex("supplierApprovalRequired");
-    g.addVertex("theRequestPriority");
-
-    addMultipleEdges(g, "name");
-    g.addEdge(SheetType.CN, "proposedSolution");
-    g.addEdge(SheetType.CR, "theRequestPriority");
-    g.addEdge(SheetType.CN, "customerApprovalRequired");
-    g.addEdge(SheetType.CR, "customerApprovalRequired");
-    g.addEdge(SheetType.CN, "supplierApprovalRequired");
-    g.addEdge(SheetType.CR, "supplierApprovalRequired");
-  }
   @Bean
   CommandLineRunner cnPersist(SheetSourceRepository sheetSourceRepository) {
-    populateGraph(g);
     return args -> {
       final var list = new ArrayList<SheetSource>();
       final List<Enum<SheetType>> cn = List.of(SheetType.CN);
@@ -79,16 +32,13 @@ public class CNConfiguration {
       final List<Enum<SheetType>> cnAndCr = Stream.concat(cn.stream(), cr.stream()).parallel()
           .collect(Collectors.toList());
       list.add(new SheetSource("id", "infoPageIdentityDisplayType", String.class.getTypeName(),
-              converter(g ,"name")));
-
-
-      list.add(new SheetSource("name", String.class.getTypeName(), converter(g ,"name")));
-      list.add(new SheetSource("proposedSolution", String.class.getTypeName(), converter(g ,"proposedSolution")));
-      list.add(new SheetSource("customerApprovalRequired", Boolean.class.getTypeName(), converter(g ,"customerApprovalRequired")));
-      list.add(new SheetSource("supplierApprovalRequired", Boolean.class.getTypeName(), converter(g ,"supplierApprovalRequired")));
-      list.add(new SheetSource("theRequestPriority", String.class.getTypeName(), converter(g ,"theRequestPriority")));
+          cnAndCr));
+      list.add(new SheetSource("name", String.class.getTypeName(), cnAndCr));
+      list.add(new SheetSource("proposedSolution", String.class.getTypeName(), cn));
+      list.add(new SheetSource("customerApprovalRequired", Boolean.class.getTypeName(), cnAndCr));
+      list.add(new SheetSource("supplierApprovalRequired", Boolean.class.getTypeName(), cnAndCr));
+      list.add(new SheetSource("theRequestPriority", String.class.getTypeName(), cr));
       sheetSourceRepository.saveAll(list);
     };
   }
-
 }
