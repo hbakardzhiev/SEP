18c24f6144cee8db1dbf5da794fbdde1397e960d
Ne184 <nevena.gincheva@abv.bg>
2022-05-16 16:26:24 +0200

    Adding the actionType

--- src/main/java/com/example/demo/configuration/CheckConfig.java
+++ src/main/java/com/example/demo/configuration/CheckConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.configuration;
 
 import com.example.demo.modules.*;
+import com.example.demo.repository.ActionValueTypeRepository;
 import com.example.demo.repository.CheckRepository;
 import com.example.demo.repository.DemoRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -14,7 +15,7 @@ import java.util.List;
 public class CheckConfig {
 
     @Bean
-    CommandLineRunner commandLineCheck(CheckRepository repository) {
+    CommandLineRunner commandLineCheck(CheckRepository repository, ActionValueTypeRepository repository2) {
 //        Check2 check1 = new Check2("check1", "CN", "name", "abc");
 
         return args -> {
@@ -22,12 +23,24 @@ public class CheckConfig {
                     "CN",
                     "name", null);
 
+            ActionValueType actionType = new ActionValueType("NotEmpty", null);
+            actionType.add(firstCheck);
             Check2 secondCheck = new Check2("Check 2",
                     "CT",
                     "description",
-                    "NotContains");
+                    "banana");
+            ActionValueType actionType2 = new ActionValueType("Contains", "String");
+            actionType2.add(secondCheck);
+
+            Check2 thirdCheck = new Check2("Check 3",
+                    "CT",
+                    "description",
+                    "pineapple");
+            ActionValueType actionType3 = new ActionValueType("Contains", "String");
+            actionType3.add(thirdCheck);
+
             repository.saveAll(
-                    List.of(firstCheck, secondCheck));
+                    List.of(firstCheck, secondCheck, thirdCheck));
         };
     }
 
