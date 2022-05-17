15a7da7fab43a7324d30339724b333135ee2e28f
ynngdd <yaskata123@gmail.com>
2022-05-17 11:29:25 +0200

    incorporated abstract data structure to handle decoupling of attributes

--- src/main/java/com/example/demo/configuration/Configuration.java
+++ src/main/java/com/example/demo/configuration/Configuration.java
@@ -9,12 +9,11 @@ import com.example.demo.modules.SheetType;
 import com.example.demo.repository.SheetSourceRepository;
 import com.example.demo.repository.ParserRepository;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.stream.Collectors;
+
+import org.jgrapht.graph.DefaultEdge;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 
@@ -31,33 +30,25 @@ public class Configuration {
     return new ParserBase();
   }
 
+
   @Bean
   CommandLineRunner cnPersist(SheetSourceRepository sheetSourceRepository) {
-//    var graph = new GraphConfig();
+    var graph = new GraphConfig();
+    ArrayList<CustomEdge> edges = (ArrayList<CustomEdge>) graph.getEdges();
     return args -> {
       final var list = new ArrayList<SheetSource>();
       final var types = new HashSet<SheetType>();
       types.add(SheetType.CN);
       types.add(SheetType.CR);
 
-      for (SheetType sheetType : Arrays.asList(SheetType.CN, SheetType.CR)) {
+      for (SheetType sheetType: Arrays.asList(SheetType.CN, SheetType.CR, SheetType.CT)) {
         list.add(new SheetSource("id", "infoPageIdentityDisplayType", String.class.getTypeName(),
             sheetType));
-        list.add(new SheetSource("id", "infoPageIdentityDisplayType", String.class.getTypeName(),
-            sheetType));
-        list.add(new SheetSource("name", String.class.getTypeName(), sheetType));
-        list.add(new SheetSource("customerApprovalRequired", Boolean.class.getTypeName(),
-            sheetType));
-        list.add(new SheetSource("supplierApprovalRequired", Boolean.class.getTypeName(),
-            sheetType));
       }
-      for (var cnType : Arrays.asList(SheetType.CN)) {
-        list.add(
-            new SheetSource("proposedSolution", String.class.getTypeName(), cnType));
-      }
-      for (var crType : Arrays.asList(SheetType.CR)) {
-        list.add(
-            new SheetSource("theRequestPriority", String.class.getTypeName(), crType));
+
+      for (CustomEdge e: edges) {
+        list.add(new SheetSource((String) e.getTargetCustom(), e.getTargetCustom().getClass().getTypeName(),
+                 SheetType.valueOf((String) e.getSourceCustom())));
       }
 
       sheetSourceRepository.saveAllAndFlush(list);
