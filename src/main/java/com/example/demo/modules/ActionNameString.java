7c8072ba3b297f4009aa804e0756d21289571b40
saandypp <59781108+saandypp@users.noreply.github.com>
2022-05-18 01:16:12 +0200

    When info about checks are received, they are automatically linked to the corresponding action and saved in db. Bug regarding showing infinite recursion also fixed.

---
+++ src/main/java/com/example/demo/modules/ActionNameString.java
@@ -0,0 +1,10 @@
+package com.example.demo.modules;
+
+public class ActionNameString {
+
+    private String actionName;
+
+    public String getActionName() {
+        return actionName;
+    }
+}
---
+++ src/main/java/com/example/demo/modules/CheckAndActionName.java
@@ -0,0 +1,6 @@
+package com.example.demo.modules;
+
+public class CheckAndActionName {
+    public Check2 theCheck;
+    public ActionNameString actionName;
+}
