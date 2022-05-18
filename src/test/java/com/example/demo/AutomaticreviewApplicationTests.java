de227a68f405f1366b6843c463c9a4cf4bea1608
Ne184 <nevena.gincheva@abv.bg>
2022-05-18 11:33:59 +0200

    Added in-memory h2 database

--- src/test/java/com/example/demo/AutomaticreviewApplicationTests.java
+++ src/test/java/com/example/demo/AutomaticreviewApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@DataJpaTest
 class AutomaticreviewApplicationTests {
 
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+
+  }
 }
