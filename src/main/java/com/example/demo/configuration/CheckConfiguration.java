02fae978d8daecd29d271bcafcf09183efbfbcf6
Ne184 <nevena.gincheva@abv.bg>
2022-05-29 17:18:01 +0200

    Added more checks/Everything seems working

--- src/main/java/com/example/demo/configuration/CheckConfiguration.java
+++ src/main/java/com/example/demo/configuration/CheckConfiguration.java
@@ -11,26 +11,43 @@ import java.util.List;
 /** Configuration file for adding checks to the database. */
 @Configuration
 public class CheckConfiguration {
+  @Bean
+  CommandLineRunner commandLineCheck(CheckRepository repository) {
 
-//  @Bean
-//  CommandLineRunner commandLineCheck(CheckRepository repository) {
-//
-//    return args -> {
-//      Check firstCheck =
-//          new Check("Check1", "Change Notice", "name", "null", "comment to show when the check fails ");
-//
-//      ActionValueType actionType = new ActionValueType("NotEmpty", "null", "pls1");
-//      actionType.add(firstCheck);
-//
-//      Check secondCheck = new Check("Check2", "Change Notice", "des", "1", "comment2");
-//      ActionValueType actionType2 = new ActionValueType("StrictlyGreater", "Integer", "pls2");
-//      actionType2.add(secondCheck);
-//
-//      Check thirdCheck = new Check("Check3", "Change Notice", "supplierApprovalRequired", "No", "comment3");
-//      ActionValueType actionType3 = new ActionValueType("Contains", "String", "pls3");
-//      actionType3.add(thirdCheck);
-//
-//      repository.saveAll(List.of(firstCheck, secondCheck, thirdCheck));
-//    };
-//  }
+    return args -> {
+      Check firstCheck =
+          new Check("Check1", "Change Notice", "name", "null", "comment to show when the check fails ");
+      ActionValueType actionType = new ActionValueType("NotEmpty", "null", "pls1");
+      actionType.add(firstCheck);
+
+      Check secondCheck = new Check("Check2", "Change Notice", "proposedSolution", "null", "comment2");
+      ActionValueType actionType2 = new ActionValueType("NotEmpty", "null", "pls2");
+      actionType2.add(secondCheck);
+
+      Check check6 = new Check("Check6", "Change Notice", "customerApprovalRequired", "No", "comment3");
+      ActionValueType actionType6 = new ActionValueType("IsEqual", "String", "pls3");
+      actionType6.add(check6);
+
+      Check thirdCheck = new Check("Check3", "Change Notice", "supplierApprovalRequired", "No", "comment3");
+      ActionValueType actionType3 = new ActionValueType("IsEqual", "String", "pls3");
+      actionType3.add(thirdCheck);
+
+      //Change Requests checks
+      Check forthCheck = new Check("Check4", "Change Request", "name", "null", "comment3");
+      ActionValueType actionType4 = new ActionValueType("NotEmpty", "null", "pls3");
+      actionType4.add(forthCheck);
+
+      Check check5 = new Check("Check5", "Change Request", "theRequestPriority", "null", "comment3");
+      ActionValueType actionType5 = new ActionValueType("NotEmpty", "null", "pls3");
+      actionType5.add(check5);
+
+      //put customer Approval when equal is ready
+
+      //Change Tasks checks
+
+
+      repository.saveAll(List.of(firstCheck, secondCheck, thirdCheck, forthCheck, check5, check6));
+    };
+  }
+
 }
