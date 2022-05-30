3c56a8493611486266d7d507df0d1ad7ca5cf453
Hristo <h.bakardzhiev@gmx.com>
2022-05-30 18:39:08 +0200

    cors hotfix for all http

--- src/main/java/com/example/demo/configuration/Configuration.java
+++ src/main/java/com/example/demo/configuration/Configuration.java
@@ -10,6 +10,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /** Saves the records in the database. */
 @org.springframework.context.annotation.Configuration
@@ -42,6 +44,18 @@ public class Configuration {
                 SheetType.valueOf((String) e.getSourceCustom())));
       }
       sheetSourceRepository.saveAllAndFlush(list);
+    };
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+      }
     };
   }
 }
