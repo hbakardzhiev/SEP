d6fa4b6fa43e08e56d2add941a8923bd2c01b0c7
Hristo <h.bakardzhiev@gmx.com>
2022-05-31 14:25:53 +0200

    dmr parser

--- src/main/java/com/example/demo/modules/ParserDMR.java
+++ src/main/java/com/example/demo/modules/ParserDMR.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import org.jsoup.nodes.Document;
 
 /** Creates parses that parses DMR pages */
 public class ParserDMR extends ParserBase {
@@ -13,26 +11,30 @@ public class ParserDMR extends ParserBase {
   /**
    * Takes the CT parses and parses the DMR pages
    *
-   * @param documents the parses that parses CT
+   * @param parserCT the parses that parses CT
    * @throws IOException
    */
-  public ParserDMR(Stream<Document> documents) throws IOException {
+  public ParserDMR(ParserCT parserCT) throws IOException {
     setSheetType(SheetType.CT);
-    passCN(documents);
+    passCT(parserCT);
   }
 
   /**
-   * @param documents
+   * @param parserCT
    * @throws IOException
    */
-  private void passCN(Stream<Document> documents) throws IOException {
+  private void passCT(ParserCT parserCT) throws IOException {
+    final var documents = parserCT.getDocument().values().parallelStream();
     // TODO: change the mask here
-    final var stream = documents.map(element -> element.select("a:matchesOwn(^ECT[\\d]{6})"));
+    final var stream = documents.map(element -> element.select(
+        //id=table chnagetask result and so on is the right div in the page which has as children the <a> tags
+        "[id=table__changeTask_resultingItems_table_TABLE] a:matchesOwn((^D[\\d]{9})|(^EngPartNr[\\d]{3}))"));
     final var listStrings =
         stream
             .flatMap(Collection::stream)
             .map(element -> element.attr("href"))
             .collect(Collectors.toCollection(ArrayList::new));
     this.setDocumentByUrl(listStrings.parallelStream());
+    listStrings.forEach(System.out::println);
   }
 }
