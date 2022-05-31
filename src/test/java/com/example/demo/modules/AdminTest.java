25b48770e855db5379d135c8e6a11fcdc70ac2be
b-stoilov <boyan.i.stoilov@gmail.com>
2022-05-31 12:04:22 +0200

    Fixed tests of Admin and AdminServiceImpl

--- src/test/java/com/example/demo/modules/AdminTest.java
+++ src/test/java/com/example/demo/modules/AdminTest.java
@@ -6,29 +6,18 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.Test;
 
 class AdminTest {
-  /**
-   * Methods under test:
-   *
-   * <ul>
-   *   <li>{@link Admin#Admin(String, String)}
-   *   <li>{@link Admin#setId(Long)}
-   *   <li>{@link Admin#setPassword(String)}
-   *   <li>{@link Admin#setUsername(String)}
-   *   <li>{@link Admin#getAdminRole()}
-   *   <li>{@link Admin#getId()}
-   *   <li>{@link Admin#getPassword()}
-   *   <li>{@link Admin#getUsername()}
-   * </ul>
-   */
-  @Test
-  void testConstructor() {
-    Admin actualAdmin = new Admin("janedoe", "biyan@gmai..com", "iloveyou");
-    actualAdmin.setId(123L);
-    actualAdmin.setPassword("iloveyou");
-    actualAdmin.setUsername("janedoe");
-    assertNull(actualAdmin.getAdminRole());
-    assertEquals(123L, actualAdmin.getId().longValue());
-    assertEquals("iloveyou", actualAdmin.getPassword());
-    assertEquals("janedoe", actualAdmin.getUsername());
-  }
+    /**
+     * Method under test: {@link Admin#Admin(String, String, String)}
+     */
+    @Test
+    void testConstructor() {
+        Admin actualAdmin = new Admin("janedoe", "jane.doe@example.org", "iloveyou");
+
+        assertEquals("ADMIN", actualAdmin.getAdminRole());
+        assertEquals("janedoe", actualAdmin.getUsername());
+        assertEquals("iloveyou", actualAdmin.getPassword());
+        assertNull(actualAdmin.getId());
+        assertEquals("jane.doe@example.org", actualAdmin.getEmail());
+    }
 }
+
