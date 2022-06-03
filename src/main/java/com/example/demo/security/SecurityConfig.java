40501dc6de260498528e103dd7a16dac2428aabf
Hristo <h.bakardzhiev@gmx.com>
2022-06-03 14:21:27 +0200

    Update SecurityConfig.java

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.demo.security;
 
 import com.example.demo.filters.CustomAuthenticationFilter;
 import com.example.demo.filters.CustomAuthorizationFilter;
+import java.util.Collections;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -37,6 +42,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
     http.addFilterBefore(
         new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+  }
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    final var configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Collections.singletonList(CorsConfiguration.ALL));
+    configuration.setAllowedMethods(
+        List.of("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS"));
+    final var source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
