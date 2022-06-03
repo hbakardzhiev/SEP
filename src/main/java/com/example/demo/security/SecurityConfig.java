86d0e6f3fa2dcae444e7a2eff530f55485d8c03b
Hristo <h.bakardzhiev@gmx.com>
2022-06-03 16:12:48 +0200

    Update SecurityConfig.java

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -43,18 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     http.addFilterBefore(
         new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
   }
-
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    final var configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Collections.singletonList(CorsConfiguration.ALL));
-    configuration.setAllowedMethods(List.of("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS"));
-    final var source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
-
-  @Bean
+  
   @Override
   public AuthenticationManager authenticationManagerBean() throws Exception {
     return super.authenticationManagerBean();
