9d36a912522b7cb7b38a582335f97ea25660ebf8
Ne184 <nevena.gincheva@abv.bg>
2022-06-09 11:27:18 +0200

    Added adminConfig back, I have accidently delete it, sorry

---
+++ src/main/java/com/example/demo/configuration/AdminConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.demo.configuration;
+
+import com.example.demo.modules.Admin;
+import com.example.demo.services.AdminService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AdminConfiguration {
+
+    @Bean
+    CommandLineRunner commandLineRunnerAdmin(AdminService service) {
+        return args -> {
+            Admin def_admin = new Admin("admin", "admin@phillips", "admin");
+
+            service.addAdmin(def_admin);
+        };
+    }
+}
