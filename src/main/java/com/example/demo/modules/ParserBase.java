ea5250244e1fe7f895c4c8bcfd23261694fb24a8
Hristo <h.bakardzhiev@gmx.com>
2022-06-10 08:45:16 +0200

    setup

--- src/main/java/com/example/demo/modules/ParserBase.java
+++ src/main/java/com/example/demo/modules/ParserBase.java
@@ -1,7 +1,8 @@
 package com.example.demo.modules;
 
-import com.example.demo.Util;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.AbstractMap;
 import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.HashMap;
@@ -12,6 +13,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 
 /**
  * We follow somewhat the State design pattern by creating a base abstract parser from which we
@@ -19,13 +22,20 @@ import org.jsoup.nodes.Document;
  */
 public abstract class ParserBase {
 
-  @Getter @Setter private SheetType sheetType = SheetType.CN;
+  @Getter
+  @Setter
+  private SheetType sheetType = SheetType.CN;
+
+  @Autowired
+  ResourceLoader resourceLoader;
 
   /**
    * Dictionary which holds the unique name of the page eg. CR000001, ProjectName01 - SW Tooling,
    * E0011 LocationId002 as key and the parsed HTML content as key
    */
-  @Getter @Setter private HashMap<String, Document> document = new HashMap<>();
+  @Getter
+  @Setter
+  private HashMap<String, Document> document = new HashMap<>();
 
   /**
    * Takes the stream of urls eg. locations of the webpages, goes through each of them and saves
@@ -35,22 +45,11 @@ public abstract class ParserBase {
    * @throws IOException
    */
   public void setDocumentByUrl(Stream<String> url) throws IOException {
-    document =
-        url.map(
-                element -> {
-                  final var inputStream =
-                      this.getClass().getClassLoader().getResourceAsStream(element);
-                  try {
-                    final var currentDocument = Jsoup.parse(Util.readFromInputStream(inputStream));
-                    return new AbstractMap.SimpleEntry<>(
-                        readDocumentName(currentDocument), currentDocument);
-                  } catch (IOException e) {
-                    throw new RuntimeException(e);
-                  }
-                })
-            .collect(
-                Collectors.toMap(
-                    Map.Entry::getKey, Map.Entry::getValue, (prev, next) -> next, HashMap::new));
+    document = url.map(element -> {
+      final var currentDocument = Jsoup.parse(getResourceFileAsString(element));
+      return new AbstractMap.SimpleEntry<>(readDocumentName(currentDocument), currentDocument);
+    }).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (prev, next) -> next,
+        HashMap::new));
   }
 
   /**
@@ -59,23 +58,15 @@ public abstract class ParserBase {
    * element).
    *
    * @param tag the attribute of the html tag eg. "attrid" or "id", etc.
-   * @param id the value of the tag
+   * @param id  the value of the tag
    * @return Stream of Key Value pairs which hold Key Value pairs
    */
-  private Stream<SimpleImmutableEntry<String, SimpleImmutableEntry<String, String>>>
-      parseElementByTag(String tag, String id) {
-    final var content =
-        document.entrySet().stream()
-            .map(
-                element ->
-                    new AbstractMap.SimpleImmutableEntry<>(
-                        element.getKey(),
-                        new SimpleImmutableEntry<String, String>(
-                            id,
-                            element
-                                .getValue()
-                                .select((String.format("[%s=%s]", tag, id)))
-                                .text())));
+  private Stream<SimpleImmutableEntry<String, SimpleImmutableEntry<String, String>>> parseElementByTag(
+      String tag, String id) {
+    final var content = document.entrySet().stream().map(
+        element -> new AbstractMap.SimpleImmutableEntry<>(element.getKey(),
+            new SimpleImmutableEntry<String, String>(id,
+                element.getValue().select((String.format("[%s=%s]", tag, id))).text())));
     return content;
   }
 
@@ -85,20 +76,16 @@ public abstract class ParserBase {
    *
    * @param sheetSourceStream Stream of SheetSources tells it which page to gather
    * @return key - unique name of the document, value - key, value pair that holds the id and text
-   *     value
+   * value
    * @throws IOException
    */
-  public Stream<AbstractMap.SimpleImmutableEntry<String, SimpleImmutableEntry<String, String>>>
-      parsePage(Stream<SheetSource> sheetSourceStream) throws IOException {
-    final var stream =
-        sheetSourceStream
-            .parallel()
-            // element is a row in table sheet_source
-            .filter(element -> element.getSheetSourceType().equals(this.getSheetType()))
-            .flatMap(
-                (elementToBeParsed) ->
-                    parseElementByTag(
-                        elementToBeParsed.getHtmlTag(), elementToBeParsed.getHtmlID()));
+  public Stream<AbstractMap.SimpleImmutableEntry<String, SimpleImmutableEntry<String, String>>> parsePage(
+      Stream<SheetSource> sheetSourceStream) throws IOException {
+    final var stream = sheetSourceStream.parallel()
+        // element is a row in table sheet_source
+        .filter(element -> element.getSheetSourceType().equals(this.getSheetType())).flatMap(
+            (elementToBeParsed) -> parseElementByTag(elementToBeParsed.getHtmlTag(),
+                elementToBeParsed.getHtmlID()));
     return stream;
   }
 
@@ -111,4 +98,17 @@ public abstract class ParserBase {
     stringBuilder.append(secondPart);
     return stringBuilder.toString();
   }
+
+
+  private String getResourceFileAsString(String fileName) {
+    final var resource = resourceLoader.getResource(fileName);
+    try {
+      final var is = resource.getInputStream();
+      final var reader = new BufferedReader(new InputStreamReader(is));
+      return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+    } catch (IOException exception) {
+      throw new RuntimeException("resource not found");
+    }
+  }
+
 }
