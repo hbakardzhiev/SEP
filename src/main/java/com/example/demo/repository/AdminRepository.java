8f42352ff83681d54870b7e369587e730db7c144
b-stoilov <boyan.i.stoilov@gmail.com>
2022-06-20 19:57:09 +0200

    another typo fix

--- src/main/java/com/example/demo/repository/AdminRepository.java
+++ src/main/java/com/example/demo/repository/AdminRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Repository for Admin class - enables Spring Data to find all the basic interface & automatically
  * create implementation for CRUD features regarding Admin class.
  */
-public interface AdminRepoistory extends JpaRepository<Admin, Long> {
+public interface AdminRepository extends JpaRepository<Admin, Long> {
 
   Admin findAdminByUsername(String username);
 }
