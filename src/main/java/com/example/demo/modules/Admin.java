681c091f5b56eb7190265d20ea2e1a8d4ece26ee
Hristo Bakardzhiev <h.bakardzhiev@gmx.com>
2022-07-01 09:55:40 +0200

    fix duplicate admins

--- src/main/java/com/example/demo/modules/Admin.java
+++ src/main/java/com/example/demo/modules/Admin.java
@@ -23,7 +23,7 @@ public class Admin {
   private Long id;
 
   /** Username of the administrator. */
-  @Column(name = "username")
+  @Column(name = "username", unique = true)
   private String username;
 
   /** Email address of the administrator. */
