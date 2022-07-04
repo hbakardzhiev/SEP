21e2f13ad7bddc7d789ac5e0ab696e97b87d229e
Ne184 <64256244+Ne184@users.noreply.github.com>
2022-07-04 14:40:55 +0200

    Deleted commented lines

--- src/test/java/com/example/demo/services/CheckWrapperServiceTest.java
+++ src/test/java/com/example/demo/services/CheckWrapperServiceTest.java
@@ -7,9 +7,7 @@ import com.example.demo.repository.SheetSourceRepository;
 
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -23,7 +21,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ContextConfiguration(classes = {CheckWrapperService.class})
@@ -66,33 +63,11 @@ class CheckWrapperServiceTest {
 //        given(admin.getId()).willReturn(123L);// when does not work
     }
 
-    @BeforeAll
-    static void beforeAll(){
-//        final var adminRepository = mock(AdminRepository.class);
-//        final var securityContext = mock(SecurityContext.class);
-//        final var applicationUser = mock(UserDetails.class);
-//        final var authentication = mock(Authentication.class);
-//        when(securityContext.getAuthentication()).thenReturn(authentication);
-//        SecurityContextHolder.setContext(securityContext);
-//        when(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).thenReturn(applicationUser);
-//        final var admin = new Admin();
-//        when(adminRepository.findAdminByUsername(applicationUser.toString())).thenReturn(admin);
-//        given(admin.getId()).willReturn(123L);// when does not work
-    }
 
     @Test
     void save() {
         // given
         Check checkToBeSaved = new Check("Check 1", "CN", "description", "Philips", "comment", 2L);
-//        final var securityContext = mock(SecurityContext.class);
-//        final var applicationUser = mock(UserDetails.class);
-//        final var authentication = mock(Authentication.class);
-//        when(securityContext.getAuthentication()).thenReturn(authentication);
-//        SecurityContextHolder.setContext(securityContext);
-//        when(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).thenReturn(applicationUser);
-//        final var admin = new Admin();
-//        when(adminRepository.findAdminByUsername(applicationUser.toString())).thenReturn(admin);
-//    when(admin.getId()).thenReturn(123L);
 
         // when
         checkWrapperServiceunderTest.save(checkToBeSaved);
@@ -133,7 +108,6 @@ class CheckWrapperServiceTest {
      * Method under test: {@link CheckWrapperService#extractCheck(CheckAndActionName)}
      */
     @Test
-//    @Disabled("TODO: Complete this test")
     void testExtractCheck2() {
         final var check =
                 new Check("CN_description", "CN", "description", "Philips", "comment");
@@ -179,86 +153,6 @@ class CheckWrapperServiceTest {
         when(this.actionService.findByName((String) any())).thenReturn(action);
 
         this.checkWrapperService.extractCheck(checkAndActionName);
-    }
-
-    /**
-     * Method under test: {@link CheckWrapperService#save(Check)}
-     */
-    @Test
-    @Disabled("TODO: Complete this test")
-    void testSave() throws Exception {
-        // TODO: Complete this test.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "org.springframework.security.core.Authentication.getPrincipal()" because the return value of "org.springframework.security.core.context.SecurityContext.getAuthentication()" is null
-        //       at com.example.demo.Util.getUsernameFromPrincipal(Util.java:47)
-        //       at com.example.demo.services.CheckWrapperService.addAuthor(CheckWrapperService.java:64)
-        //       at com.example.demo.services.CheckWrapperService.save(CheckWrapperService.java:58)
-        //   In order to prevent save(Check)
-        //   from throwing NullPointerException, add constructors or factory
-        //   methods that make it easier to construct fully initialized objects used in
-        //   save(Check).
-        //   See https://diff.blue/R013 to resolve this issue.
-
-        Action action = new Action();
-        action.setAction("Action");
-        action.setChecks(new ArrayList<>());
-        action.setDescription("The characteristics of someone or something");
-        action.setValueType("42");
-
-        Check check = new Check();
-        check.setActionType(action);
-        check.setAttribute("Attribute");
-        check.setAuthor(123L);
-        check.setComments("Comments");
-        check.setDocSource("Doc Source");
-        check.setName("Name");
-        check.setValue("42");
-        this.checkWrapperService.save(check);
-    }
-
-    /**
-     * Method under test: {@link CheckWrapperService#save(Check)}
-     */
-    @Test
-    @Disabled("TODO: Complete this test")
-    void testSave2() throws Exception {
-        // TODO: Complete this test.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "org.springframework.security.core.Authentication.getPrincipal()" because the return value of "org.springframework.security.core.context.SecurityContext.getAuthentication()" is null
-        //       at com.example.demo.Util.getUsernameFromPrincipal(Util.java:47)
-        //       at com.example.demo.services.CheckWrapperService.addAuthor(CheckWrapperService.java:64)
-        //       at com.example.demo.services.CheckWrapperService.save(CheckWrapperService.java:58)
-        //   In order to prevent save(Check)
-        //   from throwing NullPointerException, add constructors or factory
-        //   methods that make it easier to construct fully initialized objects used in
-        //   save(Check).
-        //   See https://diff.blue/R013 to resolve this issue.
-
-        Action action = new Action();
-        action.setAction("Action");
-        action.setChecks(new ArrayList<>());
-        action.setDescription("The characteristics of someone or something");
-        action.setValueType("42");
-        Check check = mock(Check.class);
-        doNothing().when(check).setActionType((Action) any());
-        doNothing().when(check).setAttribute((String) any());
-        doNothing().when(check).setAuthor((Long) any());
-        doNothing().when(check).setComments((String) any());
-        doNothing().when(check).setDocSource((String) any());
-        doNothing().when(check).setName((String) any());
-        doNothing().when(check).setValue((String) any());
-        check.setActionType(action);
-        check.setAttribute("Attribute");
-        check.setAuthor(123L);
-        check.setComments("Comments");
-        check.setDocSource("Doc Source");
-        check.setName("Name");
-        check.setValue("42");
-        this.checkWrapperService.save(check);
     }
 }
 
