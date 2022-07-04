c119d19138a889f0edd446be440228267d62c9e1
Hristo Bakardzhiev <h.bakardzhiev@gmx.com>
2022-07-04 12:39:57 +0200

    remove commented out lines

--- src/main/java/com/example/demo/controller/CheckController.java
+++ src/main/java/com/example/demo/controller/CheckController.java
@@ -57,10 +57,6 @@ public class CheckController {
 
     Check theCheck = checkService.findByName(name);
 
-//    if (theCheck == null) {
-//      throw new RuntimeException("Check not found " + name);
-//    }
-
     CheckAndActionName checkAndActionName = checkService.toCheckAndActionName(theCheck);
 
     return checkAndActionName;
@@ -123,57 +119,6 @@ public class CheckController {
     return theCheck;
   }
 
-//  /**
-//   * Extracts the check and the action and saves/updates the check in the db. Associates the check
-//   * with the corresponding action name from the action table. If a new type of attribute is used
-//   * which is not in the sheet_source table then the attribute as htmlId is added there as well.
-//   *
-//   * @param checkAndActionName a check and its action name
-//   * @return TODO: make it later void when tested with front end
-//   */
-//  private Check extractCheck(@RequestBody CheckAndActionName checkAndActionName) {
-//    Check theCheck = checkAndActionName.getTheCheck();
-//    String actionName = checkAndActionName.getActionName().getActionName();
-//
-//    actionService.findByName(actionName).add(theCheck);
-//
-//    checkService.save(theCheck);
-//    return theCheck;
-//  }
-
-
-
-//  private void createSheetSource(Check theCheck) {
-//    //  add a new attribute type in the sheet_source table if it does not exist already
-//    String typeOfAttribute = theCheck.getAttribute();
-//    SheetType sheetType = getSheetType(theCheck.getDocSource());
-//    if (!sheetSourceRepository.existsByHtmlIDAndSheetSourceType(typeOfAttribute, sheetType)) {
-//      SheetSource sheetSource =
-//          new SheetSource(typeOfAttribute, String.class.getTypeName(), sheetType);
-//      sheetSourceRepository.save(sheetSource);
-//    }
-//  }
-
-  /**
-   * Associates the docSource with the general sheetType that it has.
-   *
-   * @param docSource the document source whose sheetType needs to be found
-   * @return the sheetType that corresponds to the provided document source
-   */
-//  private SheetType getSheetType(String docSource) {
-//    SheetType sheetType;
-//    sheetType =
-//        switch (docSource) {
-//          case "Change Notice" -> SheetType.CN;
-//          case "Change Request" -> SheetType.CR;
-//          case "Engineering Change Task",
-//                  "Manufacturing Change Task",
-//                  "Master Data Change Task",
-//                  "Commercial Change Task" -> SheetType.CT;
-//          default -> SheetType.DMR;
-//        };
-//    return sheetType; // prone to mistakes everything which is not correct will be DMR
-//  }
 
   /**
    * Deletes the check from the database with the provided name.
